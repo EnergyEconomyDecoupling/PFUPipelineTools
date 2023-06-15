58ad9020076370bafd161c15072d3d4541e22961
Matthew Heun <matthew.heun@me.com>
2023-06-15 16:56:09 -0400

    Better test, now suppressing messages.

--- tests/testthat/test-storing.R
+++ tests/testthat/test-storing.R
@@ -11,7 +11,7 @@ test_that("release_target() works as expected", {
   expect_equal(list.files(path = tdir), character())
 
   # Check doing a release
-  yes_release <- release_target(tdir, targ = df, pin_name = "df", release = TRUE)
+  yes_release <- suppressMessages(release_target(tdir, targ = df, pin_name = "df", release = TRUE))
   expect_equal(yes_release, "df")
   expect_equal(list.files(path = tdir), "df")
   recursed <- list.files(path = tdir, recursive = TRUE)
