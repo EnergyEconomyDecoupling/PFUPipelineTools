48afd6edf63b034f74e7da568a569f373d5cce1c
Matthew Heun <matthew.heun@me.com>
2023-06-16 13:39:46 -0400

    Now up to 100% test coverage.

--- tests/testthat/test-storing.R
+++ tests/testthat/test-storing.R
@@ -26,5 +26,37 @@ test_that("release_target() works as expected", {
 
 
 test_that("stash_cache() works as expected", {
+  # Try without a release
+  expect_equal(stash_cache(release = FALSE), "Release not requested.")
 
+  # Now try with temporary folders and a bogus target
+  tdir <- tempdir()
+  pipeline_caches_folder <- file.path(tdir, "pipeline_caches_folder")
+  dir.create(pipeline_caches_folder, recursive = TRUE)
+  cache_folder <- file.path(tdir, "cache_folder")
+  dir.create(cache_folder, recursive = TRUE)
+  df <- data.frame(names = c("A", "B"), values = c(1, 2))
+  saveRDS(df, file = file.path(cache_folder, "df.rds"))
+  res <- stash_cache(pipeline_caches_folder = pipeline_caches_folder,
+                     cache_folder = cache_folder,
+                     file_prefix = "prefix",
+                     release = TRUE)
+  expect_equal(res, "prefix")
+  # Check that a file now exists in the pipeline_caches_folder
+  yeardir <- file.path(pipeline_caches_folder, list.files(pipeline_caches_folder))
+  monthdir <- file.path(yeardir, list.files(yeardir))
+  fname <- list.files(monthdir)
+  expect_true(startsWith(fname, "prefix"))
+  expect_true(endsWith(fname, ".zip"))
+
+  # Delete all temporary files
+  unlink(tdir, recursive = TRUE, force = TRUE)
+
+  # Try again after deleting the temporary files.
+  stash_cache(pipeline_caches_folder = pipeline_caches_folder,
+                           cache_folder = cache_folder,
+                           file_prefix = "prefix",
+                           release = TRUE) |>
+    expect_error(regexp = "cannot open the connection") |>
+    expect_error(regexp = "copying of pipeline cache unsuccessful")
 })
