3f009bcce39448140fecfed7078ff393e602abd5
Matthew Heun <matthew.heun@me.com>
2026-04-09 11:14:13 -0400

    Fixed all tests for pl_filter_collect()

--- tests/testthat/test-retrieving.R
+++ tests/testthat/test-retrieving.R
@@ -726,7 +726,7 @@ test_that("pl_filter_collect() works with compressed remote tables", {
   # Check that we can get v1 back successfully
   v1_retrieved <- pl_filter_collect(db_table_name = tname,
                                     version_string = "v1.0",
-                                    index_map = index_map,
+                                    # index_map = index_map,
                                     collect = TRUE,
                                     conn = conn,
                                     matrix_class = "matrix")
